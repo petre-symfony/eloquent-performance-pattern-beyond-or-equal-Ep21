9b91d90691f013bda3e0d6945da313cc19af42c4
petrero <petrero@gmail.com>
2023-03-14 20:51:36 +0200

    21.1 Initial Files

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder {
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void {
+
+    }
+}
