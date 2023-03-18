233c572df59c60a1f49204e97339724c02870156
petrero <petrero@gmail.com>
2023-03-18 18:59:49 +0200

    23.6 If you have a large dataset definetly consider creating an expression index for your column natural sort.  However, be aware that mysql, at least at this time does not let you create an expression index with the user defined function like this. Postgress, fortunately does allow you to do this

--- database/migrations/2023_03_18_152838_create_devices_table.php
+++ database/migrations/2023_03_18_152838_create_devices_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->string('brand');
             $table->string('resolution');
             $table->timestamps();
+            $table->rawIndex('(naturalsort(name))', 'name_sort_index');
         });
     }
 
